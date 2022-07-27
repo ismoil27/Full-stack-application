03a142805c4bde10000296dd3eb0f9e96f9d22c8
MrIsmoiljon <abduraimovismoil98@gmail.com>
2022-07-27 13:24:24 +0900

    .

--- app.js
+++ app.js
@@ -7,14 +7,18 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(express.json());
+try {
+  app.use(express.json());
 
-app.use(express.urlencoded({ extended: false }));
+  app.use(express.urlencoded({ extended: false }));
 
-// Routes
-app.use("", require("./routes/blogRoutes"));
-app.use("/api/v1/setData", require("./routes/blogRoutes"));
-//app.use("/", require("./routes/blogRoutes"));
+  // Routes
+  // app.use("", require("./routes/blogRoutes"));
+  app.use("/api/v1/setData", require("./routes/blogRoutes"));
+  //app.use("/", require("./routes/blogRoutes"));
+} catch (ex) {
+  console.log(ex);
+}
 
 app.get("/", (req, res) => {
   res.send("Hello World");
