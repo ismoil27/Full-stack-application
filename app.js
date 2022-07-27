759a816476fa9391f06d516b3667205a074464e0
MrIsmoiljon <abduraimovismoil98@gmail.com>
2022-07-27 12:56:03 +0900

    .

--- app.js
+++ app.js
@@ -11,25 +11,18 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 
+// Routes
 app.use("/api/blogs", require("./routes/blogRoutes"));
 app.use("/api/v1/setData", require("./routes/blogRoutes"));
 //app.use("/", require("./routes/blogRoutes"));
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/api/vlogs", (req, res) => {
+  try {
+    res.send("Hello World");
+  } catch (err) {
+    console.log(err);
+  }
 });
-
-// app.get("/api/v1/data", (req, res) => {});
-
-// app.get("/api/v1", (req, res) => {
-//   const sqlInsert =
-//     "INSERT INTO blogs (title, snippet, body) VALUES ('john doe', 'jack ma is the owner of this blog', 'bal bla bla bla bla text') ";
-//   db.query(sqlInsert, (err, result) => {
-//     // console.log("error", err);
-//     // console.log("result", result);
-//     res.send("Hello World");
-//   });
-// });
 
 app.use(errorHandler);
 
