f23270467df05348b1edda29f066723105ea25df
MrIsmoiljon <abduraimovismoil98@gmail.com>
2022-08-29 09:05:58 +0900

    ,

--- backend/server.js
+++
@@ -1,7 +0,0 @@
-// require("dotenv").config();
-// const http = require("http");
-// const app = require("./index");
-// const server = http.createServer(app);
-// server.listen(process.env.PORT, () => {
-//   console.log(`Server listening on ${process.env.PORT}....`);
-// });
