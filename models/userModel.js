9f5d446ffab8439d18d2f55e9f33a85358434a06
@ismoiljon <ismoiljon@smart-pro.co.kr>
2022-11-21 11:20:33 +0900

    added node-cron

--- models/userModel.js
+++ models/userModel.js
@@ -1,1 +1,7 @@
-const express = require("express");
+// const express = require("express");
+
+const cron = require("node-cron");
+
+cron.schedule("*/2 * * * *", () => {
+  console.log("running a task every two seconds");
+});
---
+++ url/url.js
@@ -0,0 +1,8 @@
+const url = require("url");
+
+const reqUrl = "http://211.225.14.76:8080/server/getChatUsersList";
+
+const urlObject = url.parse(reqUrl, ture);
+
+urlObject.host; // 211.225.14.76:8080
+urlObject.pathname; // /server/getChatUsersList
