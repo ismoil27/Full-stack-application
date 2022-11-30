7c292cd072af21240d98f07ca502d4153eb2fc6e
Ismoiljon (Jon) <abduraimovismoil98@gmail.com>
2022-11-30 22:30:57 +0900

    .

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const axios = require("axios");
-
 const cron = require("node-cron");
-
-// const bodyParser = require("body-parser");
-
-// const cron = require("node-cron");
 
 const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 8080;
@@ -31,21 +25,5 @@ app.use("/api/v1/setData", require("./routes/blogRoutes"));
 app.use("/", require("./routes/blogRoutes"));
 
 app.use(errorHandler);
-
-// function getBlogs() {
-//   axios
-//     .post("http://211.225.14.76:8080/server/getChatUsersList")
-//     .then((response) => {
-//       console.log(response.data);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// }
-
-// cron.schedule("*/2 * * * * *", () => {
-//   console.log("running a task in 3 seconds");
-//   getBlogs();
-// });
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
